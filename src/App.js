9b814a2ec466988f3d66ca3584e53984f0667bc8
luisgereda <luisgereda30@hotmail.com>
2021-05-03 13:23:16 +0200

    deplyoment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Como van?
         </p>
         <a
           className="App-link"
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+hola muchachos        </a>
       </header>
     </div>
   );
